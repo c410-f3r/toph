3b5345b4afcd8da1742cf2127d9f3332ae676069
Caio <c410.f3r@gmail.com>
2020-09-27 14:28:09 -0300

    Clippy

--- toph-runtime/src/lib.rs
+++ toph-runtime/src/lib.rs
@@ -289,12 +289,12 @@ impl_runtime_apis! {
       gas_limit: u64,
       input_data: Vec<u8>,
     ) -> ContractExecResult {
-      let (exec_result, gas_consumed) = Contracts::bare_call(origin, dest.into(), value, gas_limit, input_data);
+      let (exec_result, gas_consumed) = Contracts::bare_call(origin, dest, value, gas_limit, input_data);
       match exec_result {
         Ok(v) => ContractExecResult::Success {
           flags: v.flags.bits(),
           data: v.data,
-          gas_consumed: gas_consumed,
+          gas_consumed,
         },
         Err(_) => ContractExecResult::Error,
       }
